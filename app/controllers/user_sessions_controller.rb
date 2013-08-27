1b55789ff7e14fa4a1fcbf002a871b86c0063643
Fernando Tripicchio <fernando.tripicchio@dreamstaffing.com>
2013-08-27 19:58:03 -0300

    modified pagina despues de login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -18,7 +18,7 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session]) 
     respond_to do |format|
       if @user_session.save
-        format.html { redirect_to(:users, :notice => 'Bienvenido !') }
+        format.html { redirect_to(:products, :notice => 'Bienvenido !') }
         format.xml  { render :xml => @user_session, :status => :created, :location => @user_session }
       else
          flash[:error] = "El login o el password no son correctos" 
